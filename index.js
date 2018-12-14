35444bd7225710a9e0c4a611bdc68d98bbb0c26c
Bulent Sezgin <bsezgin@Bulents-MacBook-Pro.local>
2018-12-14 14:39:03 +0300

    second commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express'),
+   router = express.Router(),
+   mainCtrl = require('../controllers/MainController');
+  router.get('/report/:groupId/:reportId', mainCtrl.embedReport);
+  module.exports = router;
