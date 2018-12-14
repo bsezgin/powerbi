432bef3f0eff286e6a02e441675d60bbb8972397
Bulent Sezgin <33131420+bsezgin@users.noreply.github.com>
2018-12-14 15:11:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const express = require('express'),
-var app = express();
-  app.set('port', (process.env.PORT || 80));   
-  router = express.Router(),
-   mainCtrl = require('../controllers/MainController');
-  router.get('/report/:groupId/:reportId', mainCtrl.embedReport);
-
-  app.listen(app.get('port'), function() {
-   console.log("Node app is running at localhost:" + app.get('port'))
-  module.exports = router;
+router = express.Router(),
+mainCtrl = require('../controllers/MainController');
+router.get('/report/:groupId/:reportId', mainCtrl.embedReport);
+module.exports = router;
   
 });
