d8cfbc8cd733f88c1bd6961692336c060d3c2f30
Bulent Sezgin <bsezgin@Bulents-MacBook-Pro.local>
2018-12-14 15:00:04 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express'),
-   router = express.Router(),
+var app = express();
+  app.set('port', (process.env.PORT || 80));   
+  router = express.Router(),
    mainCtrl = require('../controllers/MainController');
   router.get('/report/:groupId/:reportId', mainCtrl.embedReport);
+
+  app.listen(app.get('port'), function() {
+   console.log("Node app is running at localhost:" + app.get('port'))
   module.exports = router;
+  
+});
